27b64724bbd50fb6b270a3bd1227a492ecff5bd9
horsicq <horsicq@gmail.com>
2022-02-02 00:34:12 +0100

    Fix: 2022-02-02

--- dialogxfileinfoprocess.cpp
+++ dialogxfileinfoprocess.cpp
@@ -34,7 +34,7 @@ DialogXFileInfoProcess::DialogXFileInfoProcess(QWidget *pParent, QIODevice *pDev
 
     connect(g_pThread,SIGNAL(started()),g_pFileInfo,SLOT(process()));
     connect(g_pFileInfo,SIGNAL(completed(qint64)),this,SLOT(onCompleted(qint64)));
-    connect(g_pFileInfo, SIGNAL(errorMessage(QString)), this, SLOT(errorMessage(QString)));
+    connect(g_pFileInfo,SIGNAL(errorMessage(QString)),this,SLOT(errorMessage(QString)));
 
     g_pFileInfo->setData(pDevice,pModel,options);
     g_pThread->start();
