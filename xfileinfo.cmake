961f5bcb0bd9592472a6effb604789e8b6062f49
horsicq <horsicq@gmail.com>
2022-03-09 00:11:31 +0100

    Fix: 2022-03-09

--- xfileinfo.cmake
+++ xfileinfo.cmake
@@ -1,9 +1,15 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
 
 set(XFILEINFO_SOURCES
     ${XFORMATS_SOURCES}
+    ${XDEX_SOURCES}
+    ${XPDF_SOURCES}
+    ${XARCHIVES_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xfileinfo.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xfileinfoitem.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xfileinfomodel.cpp
