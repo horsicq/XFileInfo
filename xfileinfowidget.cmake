9361662a0248aaf9d351d1184596769aec061d68
horsicq <horsicq@gmail.com>
2024-03-05 08:11:47 +0100

    Update file(s): 2024-03-05

--- xfileinfowidget.cmake
+++ xfileinfowidget.cmake
@@ -7,8 +7,11 @@ set(XFILEINFOWIDGET_SOURCES
     ${XFILEINFO_SOURCES}
     ${XDIALOGPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xfileinfowidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfileinfowidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xfileinfowidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogxfileinfoprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxfileinfoprocess.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxfileinfo.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxfileinfo.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxfileinfo.ui
 )
