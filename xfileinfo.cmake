dcc4d3fc1f6bbbc6463c1ec95d881ec81fa58b17
horsicq <horsicq@gmail.com>
2024-12-06 08:35:01 +0100

    Update file(s): 2024-12-06

--- xfileinfo.cmake
+++ xfileinfo.cmake
@@ -8,8 +8,11 @@ if (NOT DEFINED XDEX_SOURCES)
     include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
     set(XFILEINFO_SOURCES ${XFILEINFO_SOURCES} ${XDEX_SOURCES})
 endif()
+if (NOT DEFINED XPDF_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
+    set(XFILEINFO_SOURCES ${XFILEINFO_SOURCES} ${XPDF_SOURCES})
+endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XPDF/xpdf.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
 
 set(XFILEINFO_SOURCES
