17314397002a5fc0e04c9e554996c215efca4213
horsicq <horsicq@gmail.com>
2024-12-07 10:49:38 +0100

    Update file(s): 2024-12-07

--- xfileinfowidget.cmake
+++ xfileinfowidget.cmake
@@ -4,6 +4,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/xfileinfo.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/xdialogprocess.cmake)
 
 set(XFILEINFOWIDGET_SOURCES
+    ${XFILEINFOWIDGET_SOURCES}
     ${XFILEINFO_SOURCES}
     ${XDIALOGPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xfileinfowidget.cpp
